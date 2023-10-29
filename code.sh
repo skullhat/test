ec9896c09a1a0ba4bacd7127177d1bb18a1bd3ba
Omer Khalid <81094346+skullhat@users.noreply.github.com>
2023-10-29 08:20:50 +0200

    Update code.sh

--- code.sh
+++ code.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat /home/challenge/gethub/flag.txt > /home/challenge/gethub/repositories/snyk-ctf-test/flag.txt
+cat /home/challenge/gethub/flag.txt > /home/challenge/gethub/repositories/test/flag.txt
